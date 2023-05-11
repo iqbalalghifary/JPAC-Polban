6e96893b1d6594692acbf6917e662d3eb86628a4
iqbalalghifary97 <iqbalalghifary97@gmail.com>
2023-05-11 16:22:18 +0700

    navbar - done

--- client/src/examples/Navbars/DefaultNavbar/Components/Navbar.js
+++ client/src/examples/Navbars/DefaultNavbar/Components/Navbar.js
@@ -16,58 +16,44 @@ function Navbar() {
     <div>
       <header className="header header-1">
         <img
-          src={require("examples/Navbars/DefaultNavbar/images/logo.png")}
-          width={40}
+          src={require("examples/Navbars/DefaultNavbar/images/logos.png")}
+          href="/#"
+          width={150}
           height={52}
         />
-        <h3>Pusat Karir POLBAN</h3>
         <nav ref={navRef}>
           <a href="/#">Lowongan Pekerjaan</a>
           <a href="/#">Perusahaan Mitra</a>
           <a href="/#">Tracer Study</a>
           <a href="/#">Help</a>
-          {/* <button
-            disabled={showNavbar}
-            type="submit"
-            style={{ width: "20%" }}
-            className="p-2 w-150"
-          >
+        </nav>
+        <nav ref={navRef} />
+        <nav ref={navRef} />
+        <nav ref={navRef} />
+        <nav ref={navRef} />
+        <nav ref={navRef} />
+        <nav ref={navRef}>
+          <button className="btn-login">
             Login
-          </button> */}
-          {/* <button className="btn-register">
-            Register
             <svg
-              width="16"
+              width="3"
               height="16"
               viewBox="0 0 16 16"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
             />
-          </button> */}
-          {/* <button type="button" className="nav-btn nav-close-btn" onClick={showNavbar}>
-            Login <FaTimes />
-          </button> */}
+          </button>
+          <button className="btn-register">
+            register
+            <svg
+              width="3"
+              height="16"
+              viewBox="0 0 16 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            />
+          </button>
         </nav>
-        <button className="btn-login">
-          Login
-          <svg
-            width="3"
-            height="16"
-            viewBox="0 0 16 16"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          />
-        </button>
-        <button className="btn-register">
-          register
-          <svg
-            width="3"
-            height="16"
-            viewBox="0 0 16 16"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          />
-        </button>
         {/* <button type="button" className="nav-btn" onClick={showNavbar}>
           Register <FaBars />
         </button> */}
@@ -94,35 +80,3 @@ function Navbar() {
 }
 
 export default Navbar;
-
-// import React from 'react';
-// import "examples/Navbars/DefaultNavbar/Styles/main.css";
-
-// const Navbar = () => {
-//   return (
-//     <div>
-//       <header className="navbar-header">
-//         <h1 className="logo">Logo</h1>
-//         <nav className="navbar">
-//           <ul className="nav-list">
-//             <li className="nav-item">Home</li>
-//             <li className="nav-item">About</li>
-//             <li className="nav-item">Contact</li>
-//           </ul>
-//         </nav>
-//       </header>
-//       <header className="navbar-header">
-//         <h1 className="logo">Logo</h1>
-//         <nav className="navbar">
-//           <ul className="nav-list">
-//             <li className="nav-item">Shop</li>
-//             <li className="nav-item">Cart</li>
-//             <li className="nav-item">Profile</li>
-//           </ul>
-//         </nav>
-//       </header>
-//     </div>
-//   );
-// };
-
-// export default Navbar;
