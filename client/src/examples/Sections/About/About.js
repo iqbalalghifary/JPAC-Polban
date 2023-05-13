8318dba087a108a977320e4d29931b57a0a9162a
Wanda Nuriza <wanda.nuriza.tif20@polban.ac.id>
2023-05-13 18:28:42 +0700

    section-menambahkan page about us-not clear

---
+++ client/src/examples/Sections/About/About.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import Section from 'examples/HOC/Section';
+// import aboutImage from '../../../assets/img/about.jpg';
+
+const about = () => {
+  return (
+    <Section id='about'>
+      <div className='container pt-2 pb-5'>
+        <div className='section-header pt-5 pb-5 text-center'>
+          <h3 className='section-title'>
+            <span>Our </span>Company
+          </h3>
+          <h6 className='section-subtitle mr-auto ml-auto'>
+            Individualized quality care that meets the total needs of the
+            patient Individualized quality care that quality care that
+            Individualized quality care that meets the total.Suatu hari saat memancing, Toba menemukan kail pancingnya menangkap ikan besar. Namun, Toba terkejut saat menemukan ikan besar itu berubah menjadi wanita cantik.
+            Toba langsung jatuh hati dan akhirnya memutuskan untuk menikahi wanita cantik tersebut. Namun, ada persyaratan, Toba tidak boleh mengungkit asal wanita itu dari seekor ikan. Akhirnya mereka menikah dan hidup sederhana serta makin lengkap saat dikaruniai seorang anak laki-laki bernama Samosir.
+            Samosir memiliki nafsu makan yang besar sehingga Toba harus bekerja ekstra untuk memenuhi kebutuhan hidup keluarganya. Suatu hari ibunya meminta Samosir membawa makan siang untuk ayahnya yang sedang bekerja di ladang. Jarak ladangnya cukup jauh membuat Samosir merasa lapar dan haus.
+            Samosir tanpa sengaja memakan makan siang yang dibawanya hingga hanya bersisa sedikit. Samosir takut jika ayahnya marah, tetapi jika dia kembali ke rumah, tidak akan ada cukup waktu. Maka pada akhirnya Samosir memilih untuk membawakan makanan kecil itu untuk ayahnya, dan benar saja, Toba marah kepada Samosir.
+            Dengan emosi Toba melempar bekal makanannya dan mengumpat kepada Samosir dengan berbicara bahwa Samosir adalah anak ikan. Tentu mendengar hal itu membuat Samosir menangis dan pulang dengan sedih.
+            Samosir menceritakan hal tersebut ke pada ibunya dan tentu saja ibunya merasa Toba telah mengingkari janji. Akhirnya ibunya menyuruh Samosir menuju atas bukit dan menunggu saja di sana. Tiba-tiba air keluar dari tempat istri Toba berdiri dan terus mengalir dengan deras dan tempat Samosir berdiri telah menjadi sebuah pulau kecil.
+          </h6>
+        </div>
+        <div className='section-content'>
+          <div className='row'>
+            <div className='col-md-12 col-lg-6 mb-3'>
+              {/* <div className='aboutImage'>
+                <img src={aboutImage} alt='about company' />
+              </div> */}
+            </div>
+            <div className='col-md-12 col-lg-6'>
+              <h3 className='about-title'>About us</h3>
+              <div className='about-description'>
+                <p>
+                  Curabitur aliquet quam id dui posuere blandit. Donec
+                  sollicitudin molestie malesuada Pellentesque ipsum id orci
+                  porta dapibus. Vivamus suscipit tortor eget felis porttitor
+                  volutpat.
+                </p>
+                <p>
+                  Neque porro quisquam est, qui dolorem ipsum quia dolor , sed
+                  quia non numquam eius modi tempora incidunt ut labore et
+                  dolore magnam aliquam quaerat voluptatem. Nisi ut aliquid ex
+                  ea commodi consequatur?
+                </p>
+                <p>
+                  Quis autem vel eum iure reprehenderit qui in ea voluptate
+                  velit esse quam nihil molestiae consequatur, vel illum qui
+                  dolorem eum fugiat quo voluptas nulla pariatur
+                </p>
+                <button className='btn btn-primary rounded-0'>Read More</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Section>
+  );
+};
+
+export default about;
---
+++ client/src/examples/Sections/Section.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+import About from 'examples/Sections/About/About.js';
+
+const Sections = () => {
+  return (
+    <Fragment>
+      <About />
+    </Fragment>
+  );
+};
+
+export default Sections;
