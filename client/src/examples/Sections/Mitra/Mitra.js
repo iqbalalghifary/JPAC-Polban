3feba78d13f2c108c090ef45ac880058eb4047e3
Wanda Nuriza <wanda.nuriza.tif20@polban.ac.id>
2023-05-19 18:57:41 +0700

    Perusahaan Mitra-Memperbaiki card pada page perusahaan mitra-Clear

--- client/src/examples/Sections/Mitra/Mitra.js
+++ client/src/examples/Sections/Mitra/Mitra.js
@@ -1,16 +1,19 @@
 import React from "react";
 
 import Section from "examples/HOC/Section";
-import Navbar from "examples/Navbars/DefaultNavbar";
-import Footer from "examples/Footer";
 
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import { Link } from 'react-router-dom';
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import { Link } from "react-router-dom";
+
+import image1 from "examples/Sections/Mitra/Images/toko.jpg"
+import image2 from "examples/Sections/Mitra/Images/toko.jpg"
+import image1 from "examples/Sections/Mitra/Images/toko.jpg"
 
 const useStyles = makeStyles({
     root: {
@@ -18,14 +21,17 @@ const useStyles = makeStyles({
       padding: '20px',
     },
     card: {
-      maxWidth: 345,
+      maxWidth: 500,
+    },
+    media: {
+      height: 200,
     },
   });
   
   const cards = [
-    { id: 1, title: 'Card 1', description: 'This is card 1 description.', link: '/page1' },
-    { id: 2, title: 'Card 2', description: 'This is card 2 description.', link: '/page2' },
-    { id: 3, title: 'Card 3', description: 'This is card 3 description.', link: '/page3' },
+    { id: 1, title: 'Card 1', description: 'This is card 1 description.',image: image1, link: '/page1' },
+    { id: 2, title: 'Card 2', description: 'This is card 2 description.',image: '/path/to/image2.jpg', link: '/page2' },
+    { id: 3, title: 'Card 3', description: 'This is card 3 description.',image: '/path/to/image3.jpg', link: '/page3' },
   ];
   
 const Mitra = () => {
@@ -33,7 +39,6 @@ const Mitra = () => {
 
     return (
     <Section id='mitra'>
-      <Navbar />
       <div className={classes.root}>
       <Grid container spacing={3}>
         {cards.map((card) => (
@@ -41,6 +46,11 @@ const Mitra = () => {
             <Card className={classes.card}>
               <Link to={card.link}>
                 <CardActionArea>
+                  <CardMedia
+                  className={classes.media}
+                  image={card.image}
+                  title={card.title}
+                  />
                   <CardContent>
                     <Typography gutterBottom variant="h5" component="h2">
                       {card.title}
@@ -56,9 +66,11 @@ const Mitra = () => {
         ))}
       </Grid>
     </div>
-      <Footer/>
     </Section>
   );
 };
 
 export default Mitra;
+
+
+
--- client/src/examples/Sections/Section.js
+++ client/src/examples/Sections/Section.js
@@ -2,14 +2,14 @@ import React, { Fragment } from 'react';
 
 import About from 'examples/Sections/About/About.js';
 import Membership from './Membership/Membership';
-import Mitra from './Mitra/Mitra';
+// import Mitra from './Mitra/Mitra';
 
 const Sections = () => {
   return (
     <Fragment>
       {/* <About /> */}
-      {/* <Membership /> */}
-      <Mitra />
+      <Membership />
+      {/* <Mitra /> */}
     </Fragment>
   );
 };
