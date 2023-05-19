ae451be058c148d53c401f32adb2820e001f9292
Wanda Nuriza <wanda.nuriza.tif20@polban.ac.id>
2023-05-19 19:04:42 +0700

    tambahan

--- client/src/examples/Sections/Mitra/Mitra.js
+++ client/src/examples/Sections/Mitra/Mitra.js
@@ -3,7 +3,6 @@ import React from "react";
 import Section from "examples/HOC/Section";
 
 import { makeStyles } from "@material-ui/core/styles";
-<<<<<<< HEAD
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 import CardActionArea from "@mui/material/CardActionArea";
@@ -67,60 +66,6 @@ const Mitra = () => {
         ))}
       </Grid>
     </div>
-=======
-import Grid from "@material-ui/core/Grid";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
-import { Link } from "react-router-dom";
-
-const useStyles = makeStyles({
-  root: {
-    flexGrow: 1,
-    padding: "20px",
-  },
-  card: {
-    maxWidth: 345,
-  },
-});
-
-const cards = [
-  { id: 1, title: "Card 1", description: "This is card 1 description.", link: "/page1" },
-  { id: 2, title: "Card 2", description: "This is card 2 description.", link: "/page2" },
-  { id: 3, title: "Card 3", description: "This is card 3 description.", link: "/page3" },
-];
-
-function Mitra() {
-  const classes = useStyles();
-
-  return (
-    <Section id="mitra">
-      <Navbar />
-      <div className={classes.root}>
-        <Grid container spacing={3}>
-          {cards.map((card) => (
-            <Grid item xs={12} sm={6} md={4} key={card.id}>
-              <Card className={classes.card}>
-                <Link to={card.link}>
-                  <CardActionArea>
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="h2">
-                        {card.title}
-                      </Typography>
-                      <Typography variant="body2" color="textSecondary" component="p">
-                        {card.description}
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                </Link>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-      </div>
-      <Footer />
->>>>>>> 64d6b66758adbfff19624f22c06b5dde9d7741c6
     </Section>
   );
 }
