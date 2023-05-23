f810707a144d3e0318c71434b4edc8b486fbbde3
Wanda Nuriza <wanda.nuriza.tif20@polban.ac.id>
2023-05-24 03:57:59 +0700

    mencoba navigasi

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
-import Navbar from "examples/Navbars/DefaultNavbar";
-import Footer from "examples/Footer";
-import Sections from "examples/Sections/Section";
+import Sections from "./examples/Sections/Section";
+import Navbar from "./examples/Navbars/DefaultNavbar";
+import Footer from "./examples/Footer";
 
 function App() {
   return (
@@ -16,3 +16,100 @@ function App() {
 }
 
 export default App;
+
+// import React from "react";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Navbar from "./examples/Navbars/DefaultNavbar";
+// import Membership from "./examples/Sections/Membership/Membership";
+// // import Footer from "./examples/Footer";
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <div>
+//         <Navbar />
+//         <Routes>
+//           <Route path="/membership" element={<Membership />} />
+//         </Routes>
+//         {/* Content lainnya */}
+//         {/* <Footer /> */}
+//       </div>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+// import React from "react";
+// import { BrowserRouter, Route } from "react-router-dom";
+// import Navbar from "./examples/Navbars/DefaultNavbar";
+// import Portal from "./examples/Sections/Portal/Portal";
+// // import LowonganPekerjaan from "./LowonganPekerjaan";
+// // import PerusahaanMitra from "./PerusahaanMitra";
+// // import TracerStudy from "./TracerStudy";
+// import Membership from "./examples/Sections/Membership/Membership";
+// // import PaymentConfirmation from "./PaymentConfirmation";
+// // import Pengumuman from "./Pengumuman";
+// // import Agenda from "./Agenda";
+// // import Galeri from "./Galeri";
+// // import AboutUs from "./AboutUs";
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <div>
+//         <Navbar />
+
+//         <Route path="/" exact component={Portal} />
+//         {/* <Route path="/lowongan-pekerjaan" component={LowonganPekerjaan} />
+//         <Route path="/perusahaan-mitra" component={PerusahaanMitra} />
+//         <Route path="/tracer-study" component={TracerStudy} /> */}
+//         <Route path="/membership" component={Membership} />
+//         {/* <Route path="/payment-confirmation" component={PaymentConfirmation} />
+//         <Route path="/pengumuman" component={Pengumuman} />
+//         <Route path="/agenda" component={Agenda} />
+//         <Route path="/galeri" component={Galeri} />
+//         <Route path="/about-us" component={AboutUs} /> */}
+//       </div>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+// import React from "react";
+// import Navbar from "./examples/Navbars/DefaultNavbar";
+
+// function App() {
+//   return (
+//     <div>
+//       <Navbar />
+//       {/* Konten aplikasi lainnya */}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// import React from "react";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+// import Portal from "./examples/Sections/Portal/Portal";
+// // import About from "./About";
+// // import Contact from "./Contact";
+// import Navbar from "./examples/Navbars/DefaultNavbar";
+
+// function App() {
+//   return (
+//     <Router>
+//       <div>
+//         <Navbar />
+
+//         <Route path="/" exact component={Portal} />
+//         {/* <Route path="/about" component={About} />
+//         <Route path="/contact" component={Contact} /> */}
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
---
+++ fe/src/examples/Sections/Portal/Portal.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Portal() {
+  return (
+    <div>
+      <h1>Welcome to Portal Page</h1>
+      <p>This is the Portal page content.</p>
+    </div>
+  );
+}
+
+export default Portal;
