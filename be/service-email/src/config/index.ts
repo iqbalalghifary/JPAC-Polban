5a26a885adcc7693dcf1ff5d26c1034cd8d448cb
AnggaGemilang <anggagemilang12@gmail.com>
2023-05-31 21:36:52 +0700

    update config

--- be/service-email/src/config/index.ts
+++ be/service-email/src/config/index.ts
@@ -5,5 +5,6 @@ dotEnv.config();
 export const config = {
   HOST: process.env.HOST,
   SENDER: process.env.SENDER,
-  PASSWORD: process.env.PASSWORD
+  PASSWORD: process.env.PASSWORD,
+  PORT: process.env.PORT
 };
