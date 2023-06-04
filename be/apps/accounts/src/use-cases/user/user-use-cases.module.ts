449e8771c06535b2d5cfddb94acffe24992775fa
AnggaGemilang <anggagemilang12@gmail.com>
2023-06-04 18:01:19 +0700

    docker & broker 50%

---
+++ be/apps/accounts/src/use-cases/user/user-use-cases.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { MongoDataServicesModule } from '../../core/db/db-data-services.module';
+import { UserUseCases } from './user.use-case';
+import { JwtModule } from '@nestjs/jwt';
+import { config } from 'src/config';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './guard/jwt.strategy';
+
+@Module({
+  imports: [MongoDataServicesModule, PassportModule, JwtModule.register({
+    secret: config.JWT_SECRET,
+    signOptions: { expiresIn: config.JWT_EXPIRED }
+  })],
+  providers: [UserUseCases, JwtStrategy],
+  exports: [UserUseCases, JwtStrategy],
+})
+export class UserUseCasesModule {}
