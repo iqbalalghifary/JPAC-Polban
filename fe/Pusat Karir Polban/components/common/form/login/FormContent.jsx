eec8c0b932fa0ab2aa932d8c024c77ba3baace04
AnggaGemilang <anggagemilang12@gmail.com>
2023-06-26 07:58:54 +0700

    done

--- fe/Pusat Karir Polban/components/common/form/login/FormContent.jsx
+++ fe/Pusat Karir Polban/components/common/form/login/FormContent.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import axios from 'axios';
 import Cookies from 'js-cookie';
